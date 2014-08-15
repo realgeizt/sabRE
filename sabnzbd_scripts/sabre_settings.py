bc3d30c512ee0ba570c66435d49409b626b8c5c7
realgeizt <>
2014-08-16 00:05:23 +0200

    Added option "RENAME_WINDOWS_EXECUTABLES"

--- sabnzbd_scripts/sabre_settings.py
+++ sabnzbd_scripts/sabre_settings.py
@@ -3,6 +3,7 @@
 scriptdir = os.path.dirname(os.path.realpath(__file__))
 
 TAR_INCLUDE_IMAGE = scriptdir + '/sabre_includeimage.jpg'
+RENAME_WINDOWS_EXECUTABLES = True
 
 # WARNING: everything configured from here on has to point to the
 #          same file as in sabRE's configuration (settings.json).
