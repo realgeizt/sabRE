5cb4ae819b00b35d609acaa6cc1f293f6d0aa858
realgeizt <>
2014-08-30 21:33:47 +0200

    Added new settings

--- sabnzbd_scripts/sabre_settings.py
+++ sabnzbd_scripts/sabre_settings.py
@@ -9,4 +9,5 @@ RENAME_WINDOWS_EXECUTABLES = True
 #          same file as in sabRE's configuration (settings.json).
 PROGRESS_FILE = '/tmp/sabre_postprocessprogress'
 PASSWORDS_FILE = scriptdir + '/../data/passes.json'
+USER_NZBS_FILE = scriptdir + '/../data/usernzbs.json'
 TAR_CONTENTS_FILE = scriptdir + '/../data/tarcontents.json'
